1488312ef215e0e084613703baedeb5abed7704c
vivian <vivian.v18723@gmail.com>
2022-08-22 23:16:34 +0100

    use useState to change values based on user interaction

--- src/components/Likes/Likes.jsx
+++ src/components/Likes/Likes.jsx
@@ -1,21 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 export default function Likes() {
   const [likes, setLikes] = useState(0);
 
   function increase() {
-    setLikes(likes + 1);
+    setLikes((prevLikes) => {
+      return prevLikes + 1;
+    });
   }
 
   function decrease() {
-    setLikes(likes - 1);
+    if (likes !== 0) {
+      setLikes((prevLikes) => {
+        return prevLikes - 1;
+      });
+    }
   }
 
   return (
     <>
       <i className="fa-solid fa-thumbs-up" onClick={increase}></i>
       <span className="likes_counter">{likes}</span>
-      <i class="fa-solid fa-thumbs-down" onClick={decrease}></i>
+      <i className="fa-solid fa-thumbs-down" onClick={decrease}></i>
     </>
   );
 }
