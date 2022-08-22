aaa52953385cf339482cced9bbfd20c7ef2634fc
vivian <vivian.v18723@gmail.com>
2022-08-22 21:31:29 +0100

    create & add info for Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer_bg">
+        <p>Copyright </p>
+      </footer>
+    );
+  }
+}
