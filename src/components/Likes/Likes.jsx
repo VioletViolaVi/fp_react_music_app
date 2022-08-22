f98ead4a8ed9e8ebdad3c163cdd1ec80c10d8d4d
vivian <vivian.v18723@gmail.com>
2022-08-22 22:34:05 +0100

    change class to function to enable useState increase & decrease likes counter using useState

--- src/components/Likes/Likes.jsx
+++ src/components/Likes/Likes.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default class Likes extends React.Component {
-  render() {
-    return (
-      <>
-        <i className="fa-solid fa-thumbs-up">
-          <span className="likes_counter">0</span>
-        </i>
-      </>
-    );
+export default function Likes() {
+  const [likes, setLikes] = useState(0);
+
+  function increase() {
+    setLikes(likes + 1);
   }
+
+  function decrease() {
+    setLikes(likes - 1);
+  }
+
+  return (
+    <>
+      <i className="fa-solid fa-thumbs-up" onClick={increase}></i>
+      <span className="likes_counter">{likes}</span>
+      <i class="fa-solid fa-thumbs-down" onClick={decrease}></i>
+    </>
+  );
 }
