89a50b35453085465d3a9e7e1c4c05df56898aad
vivian <vivian.v18723@gmail.com>
2022-08-23 13:57:04 +0100

    reduce code to only hold elements with respective props values

--- src/components/Artists/Artists.jsx
+++ src/components/Artists/Artists.jsx
@@ -1,75 +1,42 @@
 import React from "react";
 import Likes from "../Likes/Likes";
-import mariahImg from "../../assets/mariah.jfif";
-import rihannaImg from "../../assets/rihanna.png";
-import toniImg from "../../assets/toni.jpg";
 
-const allSingersInfo = [
-  {
-    id: 1,
-    singerName: "Mariah Carey",
-    musicType: "R&B",
-    intro:
-      "Mariah Carey is an American singer, songwriter, actress, and record producer. Referred to as the 'Songbird Supreme' and the 'Queen of Christmas', she is known for her five-octave vocal range, melismatic singing style, and signature use of the whistle register.",
-    songName: "Against All Odds (Take a Look at Me Now)",
-    releaseDate: "29 May 2000",
-    coverArt: mariahImg,
-    imgAlt: "Mariah Carey cover art"
-  },
-  {
-    id: 2,
-    singerName: "Rihanna",
-    musicType: "R&B",
-    intro:
-      "Robyn Rihanna Fenty born February 20, 1988) is a Barbadian singer, actress, and businesswoman. Born in Saint Michael and raised in Bridgetown, Barbados, Rihanna was discovered by American record producer Evan Rogers who invited her to the United States to record demo tapes.",
-    songName: "Diamonds (Rihanna song)",
-    releaseDate: "September 26, 2012",
-    coverArt: rihannaImg,
-    imgAlt: "Rihanna cover art"
-  },
-  {
-    id: 3,
-    singerName: "Toni Braxton",
-    musicType: "R&B",
-    intro:
-      "Toni Michele Braxton is an American R&B singer. She has sold over 70 million records worldwide and is one of the best-selling female artists in history. Braxton has won seven Grammy Awards, nine Billboard Music Awards, seven American Music Awards, and numerous other accolades.",
-    songName: "Breathe Again",
-    releaseDate: "August 6, 1993",
-    coverArt: toniImg,
-    imgAlt: "Toni Braxton cover art"
-  }
-];
-
-export default class Artists extends React.Component {
-  render() {
-    return (
-      <>
-        {allSingersInfo.map((singleObj) => {
-          return (
-            <>
-              <section key={singleObj.id} className="artist_sec_bg">
-                <h2>{singleObj.singerName}</h2>
-                <ul className="ul_bg">
-                  <li><span className="li_sub_title">Genre: </span>{singleObj.musicType}</li>
-                  <li><span className="li_sub_title">About: </span>{singleObj.intro}</li>
-                  <li><span className="li_sub_title">Released: </span>{singleObj.releaseDate}</li>
-                  <li><span className="li_sub_title">Song: </span>{singleObj.songName}</li>
-                  <li>
-                    <img
-                      className="music_posters"
-                      src={singleObj.coverArt}
-                      alt={singleObj.imgAlt}
-                    />
-                  </li>
-                  <li>
-                    <Likes />
-                  </li>
-                </ul>
-              </section>
-            </>
-          );
-        })}
-      </>
-    );
-  }
+function Artists(props) {
+  return (
+    <>
+      <section key={props.id} className="artist_sec_bg">
+        <h2>{props.singerName}</h2>
+        <ul className="ul_bg">
+          <li>
+            <span className="li_sub_title">Genre: </span>
+            {props.musicType}
+          </li>
+          <li>
+            <span className="li_sub_title">About: </span>
+            {props.intro}
+          </li>
+          <li>
+            <span className="li_sub_title">Released: </span>
+            {props.releaseDate}
+          </li>
+          <li>
+            <span className="li_sub_title">Song: </span>
+            {props.songName}
+          </li>
+          <li>
+            <img
+              className="music_posters"
+              src={props.coverArt}
+              alt={props.imgAlt}
+            />
+          </li>
+          <li>
+            <Likes />
+          </li>
+        </ul>
+      </section>
+    </>
+  );
 }
+
+export default Artists;
