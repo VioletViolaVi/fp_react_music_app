3319d2c9563eda5fb72c8169f47b43ec36a0ba94
vivian <vivian.v18723@gmail.com>
2022-08-23 21:33:50 +0100

    move data of singers in own file

---
+++ src/pages/ForAllComponents/data.jsx
@@ -0,0 +1,38 @@
+import mariahImg from "../../assets/photos/mariah.jfif";
+import rihannaImg from "../../assets/photos/rihanna.png";
+import toniImg from "../../assets/photos/toni.jpg";
+
+const allSingersInfo = [
+  {
+    id: 1,
+    singerName: "Mariah Carey",
+    musicType: "R&B",
+    para: "Mariah Carey is an American singer, songwriter, actress, and record producer. Referred to as the 'Songbird Supreme' and the 'Queen of Christmas', she is known for her five-octave vocal range, melismatic singing style, and signature use of the whistle register.",
+    songName: "Against All Odds (Take a Look at Me Now)",
+    releaseDate: "29 May 2000",
+    coverArt: mariahImg,
+    imgAlt: "Mariah Carey cover art"
+  },
+  {
+    id: 2,
+    singerName: "Rihanna",
+    musicType: "R&B",
+    para: "Robyn Rihanna Fenty born February 20, 1988) is a Barbadian singer, actress, and businesswoman. Born in Saint Michael and raised in Bridgetown, Barbados, Rihanna was discovered by American record producer Evan Rogers who invited her to the United States to record demo tapes.",
+    songName: "Diamonds (Rihanna song)",
+    releaseDate: "September 26, 2012",
+    coverArt: rihannaImg,
+    imgAlt: "Rihanna cover art"
+  },
+  {
+    id: 3,
+    singerName: "Toni Braxton",
+    musicType: "R&B",
+    para: "Toni Michele Braxton is an American R&B singer. She has sold over 70 million records worldwide and is one of the best-selling female artists in history. Braxton has won seven Grammy Awards, nine Billboard Music Awards, seven American Music Awards, and numerous other accolades.",
+    songName: "Breathe Again",
+    releaseDate: "August 6, 1993",
+    coverArt: toniImg,
+    imgAlt: "Toni Braxton cover art"
+  }
+];
+
+export default allSingersInfo;
