a838fdb9e5193e54888f02c2e46159f4d2c0c4b4
vivian <vivian.v18723@gmail.com>
2022-08-23 20:09:56 +0100

    organise components in own folder for easier exporting

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from "react";
+
+export { default as Artists } from "./Artists";
+export { default as Footer } from "./Footer";
+export { default as NavBar } from "./NavBar";
+export { default as Search } from "./Search";
