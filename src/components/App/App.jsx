df2a0a0ff29637b4b757ada17babfbb2cef35a85
vivian <vivian.v18723@gmail.com>
2022-08-23 13:56:31 +0100

    add useEffect & useState change file path names add images via imports add arr of objs for artists use useEffect when searching for singer add function to set user's input map through arr of objs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import Artists from "../Artists/Artists";
 import Footer from "../Footer/Footer";
-import Lyrics from "../Lyrics/Lyrics";
 import NavBar from "../NavBar/NavBar";
+import Search from "../Search/Search";
+import mariahImg from "../../assets/mariah.jfif";
+import rihannaImg from "../../assets/rihanna.png";
+import toniImg from "../../assets/toni.jpg";
+
+const allSingersInfo = [
+  {
+    id: 1,
+    singerName: "Mariah Carey",
+    musicType: "R&B",
+    intro:
+      "Mariah Carey is an American singer, songwriter, actress, and record producer. Referred to as the 'Songbird Supreme' and the 'Queen of Christmas', she is known for her five-octave vocal range, melismatic singing style, and signature use of the whistle register.",
+    songName: "Against All Odds (Take a Look at Me Now)",
+    releaseDate: "29 May 2000",
+    coverArt: mariahImg,
+    imgAlt: "Mariah Carey cover art"
+  },
+  {
+    id: 2,
+    singerName: "Rihanna",
+    musicType: "R&B",
+    intro:
+      "Robyn Rihanna Fenty born February 20, 1988) is a Barbadian singer, actress, and businesswoman. Born in Saint Michael and raised in Bridgetown, Barbados, Rihanna was discovered by American record producer Evan Rogers who invited her to the United States to record demo tapes.",
+    songName: "Diamonds (Rihanna song)",
+    releaseDate: "September 26, 2012",
+    coverArt: rihannaImg,
+    imgAlt: "Rihanna cover art"
+  },
+  {
+    id: 3,
+    singerName: "Toni Braxton",
+    musicType: "R&B",
+    intro:
+      "Toni Michele Braxton is an American R&B singer. She has sold over 70 million records worldwide and is one of the best-selling female artists in history. Braxton has won seven Grammy Awards, nine Billboard Music Awards, seven American Music Awards, and numerous other accolades.",
+    songName: "Breathe Again",
+    releaseDate: "August 6, 1993",
+    coverArt: toniImg,
+    imgAlt: "Toni Braxton cover art"
+  }
+];
 
 export default function App() {
   const titles = [{ id: 1, name: "Singers" }];
+
+  const [showData, setShowData] = useState([]);
+  const [enteredUserStr, setEnteredUserStr] = useState("");
+
+  useEffect(() => {
+    async function searchSinger() {
+      const result = await allSingersInfo;
+      console.log("result ==>", result);
+      setShowData(result);
+    }
+    searchSinger();
+  }, [enteredUserStr]);
+
+  function handleUserSearch(userInput) {
+    console.log("userInput ==>", userInput);
+    setEnteredUserStr(userInput);
+  }
 
   return (
     <>
       <NavBar />
       <main>
+        <Search handleUserSubmittedSearch={handleUserSearch} />
         <h1 className="main_title">{titles[0].name}</h1>
-        <Artists />
-        <Lyrics />
+        {showData.map((singleObj) => {
+          return (
+            <Artists key={singleObj.id} singerName={singleObj.singerName} />
+          );
+        })}
       </main>
       <Footer />
     </>
