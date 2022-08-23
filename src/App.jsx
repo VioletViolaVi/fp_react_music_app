4dec17271ddd62aeb61af3d1d5d31905e4d1461a
vivian <vivian.v18723@gmail.com>
2022-08-23 20:09:00 +0100

    import deconstructed components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import Artists from "./components/Artists";
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
-import Search from "./components/Search";
+import { Artists, Footer, NavBar, Search } from "./components";
 import mariahImg from "./assets/mariah.jfif";
 import rihannaImg from "./assets/rihanna.png";
 import toniImg from "./assets/toni.jpg";
@@ -13,8 +10,7 @@ const allSingersInfo = [
     id: 1,
     singerName: "Mariah Carey",
     musicType: "R&B",
-    para:
-      "Mariah Carey is an American singer, songwriter, actress, and record producer. Referred to as the 'Songbird Supreme' and the 'Queen of Christmas', she is known for her five-octave vocal range, melismatic singing style, and signature use of the whistle register.",
+    para: "Mariah Carey is an American singer, songwriter, actress, and record producer. Referred to as the 'Songbird Supreme' and the 'Queen of Christmas', she is known for her five-octave vocal range, melismatic singing style, and signature use of the whistle register.",
     songName: "Against All Odds (Take a Look at Me Now)",
     releaseDate: "29 May 2000",
     coverArt: mariahImg,
@@ -24,8 +20,7 @@ const allSingersInfo = [
     id: 2,
     singerName: "Rihanna",
     musicType: "R&B",
-    para:
-      "Robyn Rihanna Fenty born February 20, 1988) is a Barbadian singer, actress, and businesswoman. Born in Saint Michael and raised in Bridgetown, Barbados, Rihanna was discovered by American record producer Evan Rogers who invited her to the United States to record demo tapes.",
+    para: "Robyn Rihanna Fenty born February 20, 1988) is a Barbadian singer, actress, and businesswoman. Born in Saint Michael and raised in Bridgetown, Barbados, Rihanna was discovered by American record producer Evan Rogers who invited her to the United States to record demo tapes.",
     songName: "Diamonds (Rihanna song)",
     releaseDate: "September 26, 2012",
     coverArt: rihannaImg,
@@ -35,8 +30,7 @@ const allSingersInfo = [
     id: 3,
     singerName: "Toni Braxton",
     musicType: "R&B",
-    para:
-      "Toni Michele Braxton is an American R&B singer. She has sold over 70 million records worldwide and is one of the best-selling female artists in history. Braxton has won seven Grammy Awards, nine Billboard Music Awards, seven American Music Awards, and numerous other accolades.",
+    para: "Toni Michele Braxton is an American R&B singer. She has sold over 70 million records worldwide and is one of the best-selling female artists in history. Braxton has won seven Grammy Awards, nine Billboard Music Awards, seven American Music Awards, and numerous other accolades.",
     songName: "Breathe Again",
     releaseDate: "August 6, 1993",
     coverArt: toniImg,
