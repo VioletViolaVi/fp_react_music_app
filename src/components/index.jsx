d5cf823ea224f900765ca568baf543bba493baf9
vivian <vivian.v18723@gmail.com>
2022-08-24 17:32:34 +0100

    export destructured components using alias

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,3 @@
-export { default as Footer } from "./Footer";
 export { default as NavBar } from "./Header/NavBar";
-export { default as EveryMain } from "./Main/EveryMain";
+export { default as HomeMain } from "./Main/HomeMain";
+export { default as Footer } from "./Footer";
