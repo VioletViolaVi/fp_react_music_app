3b4ccf474dece93d41bea530a88ca9cbcd6e7a6f
vivian <vivian.v18723@gmail.com>
2022-08-24 13:59:59 +0100

    export all components in Main folder... ...under 1 component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,3 @@
-export { default as Artists } from "./Main/Artists";
 export { default as Footer } from "./Footer";
 export { default as NavBar } from "./Header/NavBar";
-export { default as Search } from "./Main/Search";
-export { default as Title } from "./Main/Title";
+export { default as EveryMain } from "./Main/EveryMain";
