908a288581ac6f1dff9bea192fef99bf378d992a
vivian <vivian.v18723@gmail.com>
2022-08-24 17:35:40 +0100

    create & export LyricsComponents component

---
+++ src/pages/LyricsComponents/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Footer, NavBar } from "../../components";
+
+function LyricsComponents() {
+  return (
+    <>
+      <NavBar />
+      <h1>Lyrics go here...</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default LyricsComponents;
