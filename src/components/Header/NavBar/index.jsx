f08ed57060d1736a5de425cba41fb2e7a3ea9909
vivian <vivian.v18723@gmail.com>
2022-08-24 14:00:57 +0100

    import & use NavLink & Outlet... ...from react-router-dom & use in nav bar

--- src/components/Header/NavBar/index.jsx
+++ src/components/Header/NavBar/index.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
 
-export default class NavBar extends React.Component {
-  render() {
-    return (
+export default function NavBar() {
+  return (
+    <>
       <nav className="nav_bar">
-        <a href="#">Home</a>
-        <a href="#">Singers</a>
-        <a href="#">Contact</a>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="singers">Singers</NavLink>
+        <NavLink to="contact">Contact</NavLink>
+        <Outlet />
       </nav>
-    );
-  }
+    </>
+  );
 }
+
+// <nav className="nav_bar">
+//   <a href="#">Home</a>
+//   <a href="#">Singers</a>
+//   <a href="#">Contact</a>
+// </nav>
