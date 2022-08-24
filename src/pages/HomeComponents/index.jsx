4ad5f0570aef7f30b1a9fc94d93e795b2ad0670f
vivian <vivian.v18723@gmail.com>
2022-08-24 17:35:50 +0100

    create & export HomeComponents component

---
+++ src/pages/HomeComponents/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { HomeMain, Footer, NavBar } from "../../components";
+
+function HomeComponents() {
+  return (
+    <>
+      <NavBar />
+      <HomeMain />
+      <Footer />
+    </>
+  );
+}
+
+export default HomeComponents;
