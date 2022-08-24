0a3519c2bbbe06a7013891ffb248ea7590542725
vivian <vivian.v18723@gmail.com>
2022-08-24 16:46:37 +0100

    import Routes & Route add Routes & Route to give url paths... ...for home page & 'page not found' if page not present

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import { ForAllComponents } from "./pages";
 
 export default function App() {
   return (
     <>
-      <ForAllComponents />
+      <Routes>
+        <Route path="/" element={<ForAllComponents />}></Route>
+        <Route path="*" element={<h1>Page Not Found 404!</h1>}></Route>
+      </Routes>
     </>
   );
 }
