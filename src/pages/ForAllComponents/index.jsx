d035cabddae5b56cb44dcc7c089a4e4eb8726c96
vivian <vivian.v18723@gmail.com>
2022-08-24 16:44:46 +0100

    revert file back so... ...components exists without Routes

--- src/pages/ForAllComponents/index.jsx
+++ src/pages/ForAllComponents/index.jsx
@@ -1,24 +1,13 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 
 import { EveryMain, Footer, NavBar } from "../../components";
 
 export default function ForAllComponents() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<NavBar />}>
-          <Route index element={<EveryMain />}></Route>
-          <Route path="contact" element={<h2>Contacts go here</h2>}></Route>
-          <Route index element={<Footer />}></Route>
-        </Route>
-
-        <Route path="*" element={<h1>Page Not Found 404!</h1>}></Route>
-      </Routes>
-
-      {/* <NavBar />
+      <NavBar />
       <EveryMain />
-      <Footer /> */}
+      <Footer />
     </>
   );
 }
