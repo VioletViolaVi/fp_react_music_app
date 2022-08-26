11e9837ddf344be89e734d6e912e21e0499be2dd
vivian <vivian.v18723@gmail.com>
2022-08-26 17:23:50 +0100

    change names of imported components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import { AboutUsComponents, HomeComponents, LyricsComponents } from "./pages";
+import { AboutUsPage, HomePage, LyricsPage } from "./pages";
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomeComponents />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
 
-        <Route path="lyrics" element={<LyricsComponents />}></Route>
+        <Route path="lyrics" element={<LyricsPage />}></Route>
 
-        <Route path="about" element={<AboutUsComponents />}></Route>
+        <Route path="about" element={<AboutUsPage />}></Route>
 
         <Route path="*" element={<h1>Page Not Found 404!😭</h1>}></Route>
       </Routes>
