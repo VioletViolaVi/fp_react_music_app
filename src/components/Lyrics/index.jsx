33eb4c61e058923c66948375097f66abb3ed5f28
vivian <vivian.v18723@gmail.com>
2022-08-26 18:11:28 +0100

    add form for Lyrics component

--- src/components/Lyrics/index.jsx
+++ src/components/Lyrics/index.jsx
@@ -1,24 +1,46 @@
 import React, { useEffect, useState } from "react";
 
 function Lyrics() {
-  const [showLyrics, setShowLyrics] = useState([]);
-  const [enterSinger, setEnteredSinger] = useState("");
-  const [enterSong, setEnteredSong] = useState("");
+  // const [showLyrics, setShowLyrics] = useState([]);
+  // const [enterSinger, setEnteredSinger] = useState("");
+  // const [enterSong, setEnteredSong] = useState("");
 
-  useEffect(() => {
-    fetch(
-      `https://private-451fd-lyricsovh.apiary-mock.com/v1/${enterSinger}/${enterSong}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => console.log(err));
-  }, [enterSinger, enterSong]);
+  // useEffect(() => {
+  //   fetch(
+  //     `https://private-451fd-lyricsovh.apiary-mock.com/v1/${enterSinger}/${enterSong}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, [enterSinger, enterSong]);
 
   return (
     <>
-      <h1>Lyrics go here...</h1>
+      <form>
+        <label htmlFor="artist"></label>
+        <input
+          type="text"
+          name="artist"
+          id="artist"
+          placeholder="Name of artist"
+        />
+
+        <br />
+
+        <label htmlFor="songTitle"></label>
+        <input
+          type="text"
+          name="songTitle"
+          id="songTitle"
+          placeholder="Name of song"
+        />
+
+        <br />
+
+        <button>Get Lyrics</button>
+      </form>
     </>
   );
 }
