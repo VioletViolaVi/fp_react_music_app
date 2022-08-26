76eaf888b94e12a1f05f8df7662468d5edc47cf3
vivian <vivian.v18723@gmail.com>
2022-08-26 17:07:37 +0100

    move NavBar component to root... ...level inside components folder

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,4 @@
-export { default as NavBar } from "./Header/NavBar";
+export { default as NavBar } from "./NavBar";
 export { default as Footer } from "./Footer";
 export { default as HomeMain } from "./Main/HomeMain";
 export { default as LyricsMain } from "./Main/LyricsMain";
