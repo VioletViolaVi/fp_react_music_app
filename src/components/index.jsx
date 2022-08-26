03debec7b659ea5cb1927354e40b23567615a53d
vivian <vivian.v18723@gmail.com>
2022-08-26 18:05:42 +0100

    export use BackBtn component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
+export { default as BackBtn } from "./BackBtn";
 export { default as NavBar } from "./NavBar";
 export { default as Home } from "./Home";
 export { default as Lyrics } from "./Lyrics";
