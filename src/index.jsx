67608cad76471861d8cdcf4f0b50517bfe3ed597
vivian <vivian.v18723@gmail.com>
2022-08-26 18:05:28 +0100

    import & use BackBtn component

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
-import { Footer, NavBar } from "./components";
+import { BackBtn, Footer, NavBar } from "./components";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <NavBar />
+      <BackBtn />
       <App />
       <Footer />
     </Router>
