1b90a920769793d1e52a47a0a156372dc8fbb529
vivian <vivian.v18723@gmail.com>
2022-08-26 17:25:24 +0100

    change file location of components for home page

---
+++ src/components/Home/Artists/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import Likes from "../Likes";
+
+function Artists(props) {
+  return (
+    <section key={props.id} className="artist_sec_bg">
+      <h2>{props.singerName}</h2>
+      <ul className="ul_bg">
+        <li>
+          <span className="li_sub_title">Genre: </span>
+          {props.musicType}
+        </li>
+        <li>
+          <span className="li_sub_title">About: </span>
+          {props.para}
+        </li>
+        <li>
+          <span className="li_sub_title">Released: </span>
+          {props.releaseDate}
+        </li>
+        <li>
+          <span className="li_sub_title">Song: </span>
+          {props.songName}
+        </li>
+        <li>
+          <img
+            className="music_posters"
+            src={props.coverArt}
+            alt={props.imgAlt}
+          />
+        </li>
+        <li>
+          <Likes />
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Artists;
---
+++ src/components/Home/Title/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Title() {
+  return (
+    <>
+      <h1 className="main_title">Singers</h1>
+    </>
+  );
+}
