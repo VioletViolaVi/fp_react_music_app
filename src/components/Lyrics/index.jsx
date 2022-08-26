2cb592139fdef7765f52be7bd432523883f49971
vivian <vivian.v18723@gmail.com>
2022-08-26 21:53:44 +0100

    implement useEffect & useState

--- src/components/Lyrics/index.jsx
+++ src/components/Lyrics/index.jsx
@@ -1,30 +1,41 @@
 import React, { useEffect, useState } from "react";
+import LyricsCard from "./LyricsCard";
 
-function Lyrics() {
-  // const [showLyrics, setShowLyrics] = useState([]);
-  // const [enterSinger, setEnteredSinger] = useState("");
-  // const [enterSong, setEnteredSong] = useState("");
+const API_URL = "https://api.lyrics.ovh/v1/";
 
-  // useEffect(() => {
-  //   fetch(
-  //     `https://private-451fd-lyricsovh.apiary-mock.com/v1/${enterSinger}/${enterSong}`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, [enterSinger, enterSong]);
+const Lyrics = () => {
+  const [showLyrics, setShowLyrics] = useState([]);
+  const [enterSinger, setEnteredSinger] = useState("");
+  const [enterSong, setEnteredSong] = useState("");
 
+  // its own function
+  const searchLyricsAPI = async (singer, song) => {
+    const res = await fetch(`${API_URL}/${singer}/${song}`);
+    const data = await res.json();
+
+    setShowLyrics(data);
+    console.log("data ==> ", data);
+  };
+
+  //  fetching data
+  useEffect(() => {
+    searchLyricsAPI("rihanna", "disturbia");
+  }, []);
+
+  // user interface
   return (
     <>
       <form>
-        <label htmlFor="artist"></label>
+        <label htmlFor="singer"></label>
         <input
           type="text"
-          name="artist"
-          id="artist"
-          placeholder="Name of artist"
+          name="singer"
+          id="singer"
+          placeholder="Name of singer"
+          value={enterSinger}
+          onChange={(e) => {
+            return setEnteredSinger(e.target.value);
+          }}
         />
 
         <br />
@@ -35,14 +46,36 @@ function Lyrics() {
           name="songTitle"
           id="songTitle"
           placeholder="Name of song"
+          value={enterSong}
+          onChange={(e) => {
+            return setEnteredSong(e.target.value);
+          }}
         />
 
         <br />
 
-        <button>Get Lyrics</button>
+        <button
+          onClick={() => {
+            return searchLyricsAPI(enterSinger, enterSong);
+          }}
+        >
+          Get Lyrics
+        </button>
       </form>
+
+      <section>
+        {showLyrics.map((singleObj) => {
+          return (
+            <LyricsCard
+              key={singleObj.id}
+              lyrics={singleObj.lyrics}
+              artist={singleObj.artist}
+            />
+          );
+        })}
+      </section>
     </>
   );
-}
+};
 
 export default Lyrics;
